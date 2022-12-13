fb1ecc3b0b1cc2745eb9648e921c04393d147ccf
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-14 01:24:13 +0300

    feat: update input component

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,21 +1,35 @@
-import React from "react";
-
+import React, { HTMLInputTypeAttribute } from "react";
+import { Field, ErrorMessage } from "formik";
+import classnames from "classnames";
 interface IProps {
-  leftIcon?: React.ReactNode;
-  rightIcon?: React.ReactNode;
+  name: string;
   placeholder?: string;
   label?: string;
+  type?: HTMLInputTypeAttribute;
+  errorText?: string;
+  renderLeftIcon?: React.ReactNode;
+  onBlur?: React.ChangeEvent<HTMLInputElement>;
+  onChange?: React.ChangeEvent<HTMLInputElement>;
 }
 
-const Input: React.FC<IProps> = ({ leftIcon, rightIcon, label, placeholder }) => {
+const Input: React.FC<IProps> = ({ name, type = "text", errorText, renderLeftIcon, label, placeholder }) => {
   return (
     <div className="w-full">
-      {label && <label>{label}</label>}
-      <div className="flex items-center border-tertiary border-[1px] rounded-lg py-2 px-3">
-        {leftIcon}
-        <input className="bg-transparent w-full outline-none text-placeholder placeholder:text-placeholder" type="text" placeholder={placeholder} />
-        {rightIcon}
+      <div
+        className={classnames("flex items-center border-tertiary border-[1px] rounded-lg py-4 px-3 space-x-2", {
+          "border-red-500": errorText,
+        })}
+      >
+        <label htmlFor={name}>{label}</label>
+        {renderLeftIcon}
+        <Field
+          className="bg-transparent w-full outline-none text-placeholder placeholder:text-placeholder"
+          name={name}
+          type={type}
+          placeholder={placeholder}
+        />
       </div>
+      <ErrorMessage className="pt-1 text-sm text-red-500" name={name} component="p" />
     </div>
   );
 };
