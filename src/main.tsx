581fcd86c4c9cb3e484a6c2a358064356cd8e871
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-13 20:22:46 +0300

    feat: add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "@/router/index";
 
+import "@/styles/tailwind.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RouterProvider router={Router} />
 );
