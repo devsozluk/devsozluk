e5224217db06405a4a12b10a5643272ac2a30a47
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-14 01:22:51 +0300

    feat: update header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import Input from "@/components/UI/Input/index";
 import Icon from "@/components/UI/Icon/index";
-import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <div className="border-primary border-t-4 px-8 py-3 flex flex-col md:flex-row md:justify-between space-y-3 md:space-y-0 items-center">
-      <div>
+    <div className="fixed left-0 right-0 border-primary border-t-4 px-8 py-3 flex flex-col md:flex-row md:justify-between space-y-3 md:space-y-0 items-center">
+      <Link to="/">
         <h1 className="text-primary font-bold text-3xl">DevLab.</h1>
-      </div>
+      </Link>
       <div className="w-full md:w-[400px]">
-        <Input placeholder="Aramak istediğiniz konuyu yazın." rightIcon={<Icon icon="search" width={20} height={20} />} />
+        {/* <Input placeholder="Aramak istediğiniz konuyu yazın." rightIcon={<Icon icon="search" width={20} height={20} />} /> */}
       </div>
       <div className="space-x-2">
         <Link to="/auth/login">giriş</Link>
