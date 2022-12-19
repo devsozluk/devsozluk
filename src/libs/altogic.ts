1cf8dcd10c22aa2ecdd0e0247d97c8526fe0a8d3
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-19 03:17:19 +0300

    feat: update altogic configuration

--- src/libs/altogic.ts
+++ src/libs/altogic.ts
@@ -1,7 +1,5 @@
 import { createClient } from "altogic";
 
-const altogic = createClient(import.meta.env.VITE_ALTOGIC_ENV_URL, import.meta.env.VITE_ALTOGIC_CLIENT_KEY, {
-  signInRedirect: "/sign-in",
-});
+const altogic = createClient(import.meta.env.VITE_ALTOGIC_ENV_URL, import.meta.env.VITE_ALTOGIC_CLIENT_KEY, {});
 
 export default altogic;
