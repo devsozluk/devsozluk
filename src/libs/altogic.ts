e39c16a6a78393bd20c87006914c63477ed402cb
seymennklc <seymennklc@gmail.com>
2022-12-27 20:41:25 +0300

    configuration

--- src/libs/altogic.ts
+++ src/libs/altogic.ts
@@ -1,5 +1,9 @@
 import { createClient } from "altogic";
 
-const altogic = createClient(import.meta.env.VITE_ALTOGIC_ENV_URL, import.meta.env.VITE_ALTOGIC_CLIENT_KEY, {});
+const altogic = createClient(
+   import.meta.env.VITE_ALTOGIC_ENV_URL,
+   import.meta.env.VITE_ALTOGIC_CLIENT_KEY,
+   {}
+);
 
 export default altogic;
