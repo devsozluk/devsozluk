e9d62e39fbd82a8ab0730e4f662af30d4eec78fd
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-30 03:18:30 +0300

    fix: deployment errors

--- src/pages/CreateTopic/index.tsx
+++ src/pages/CreateTopic/index.tsx
@@ -4,28 +4,24 @@ import Input from "@/components/Form/Input";
 import { CreateTopicData } from "@/types";
 import { useAppSelector } from "@/utils/hooks";
 import { CreateTopicSchema } from "@/utils/schemas";
-import MDEditor from "@uiw/react-md-editor";
-import classNames from "classnames";
 import { Formik } from "formik";
 import React from "react";
 import { Form, useNavigate } from "react-router-dom";
 
 const CreateTopic: React.FC = () => {
-  const { user } = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
   const initialValues: CreateTopicData = { title: "", content: "" };
 
-  const handleCreate = async ({ title, content }: CreateTopicData, { setSubmitting }: any) => {
-    // const { data, errors } = await TopicService.CreateTopic(user?._id as string, { title, content });
-    // if (data) {
-    //   toast.success("konu oluşturuldu yönlendiriliyorsunuz...");
-    //   navigate("/konu/" + data.slug);
-    // } else {
-    //   if (errors?.items[0].code === "not_unique" && errors.items[0].details?.field === "title") return toast.error("bu başlıkta bir konu var zaten.");
-    //   toast.error(errors?.items[0].message);
-    // }
-    // setSubmitting(false);
-  };
+  // const handleCreate = async ({ title, content }: CreateTopicData, { setSubmitting }: any) => {
+  //   // const { data, errors } = await TopicService.CreateTopic(user?._id as string, { title, content });
+  //   // if (data) {
+  //   //   toast.success("konu oluşturuldu yönlendiriliyorsunuz...");
+  //   //   navigate("/konu/" + data.slug);
+  //   // } else {
+  //   //   if (errors?.items[0].code === "not_unique" && errors.items[0].details?.field === "title") return toast.error("bu başlıkta bir konu var zaten.");
+  //   //   toast.error(errors?.items[0].message);
+  //   // }
+  //   // setSubmitting(false);
+  // };
 
   return (
     <div className="h-full flex items-center justify-center">
--- src/store/topic/topicThunk.ts
+++ src/store/topic/topicThunk.ts
@@ -2,18 +2,15 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import altogic from "@/libs/altogic";
 import { toast } from "react-toastify";
 
-export const getBySlugTopic = createAsyncThunk(
-   "topic/getBySlug",
-   async (payload: { slug: string; }, { rejectWithValue }) => {
-      const endpoint = `/topics/bySlug?slug=${payload.slug}`;
+export const getBySlugTopic = createAsyncThunk("topic/getBySlug", async (payload: { slug: string }, { rejectWithValue }) => {
+  const endpoint = `/topics/bySlug?slug=${payload.slug}`;
 
-      const { data: topicData, errors } = await altogic.endpoint.get(endpoint);
+  const { data: topicData, errors } = await altogic.endpoint.get(endpoint);
 
-      if (!topicData) {
-         toast.warning("Aradığınız konu yok veya silinmiş.");
-         return rejectWithValue("Error getting topic");
-      }
+  if (!topicData) {
+    toast.warning("Aradığınız konu yok veya silinmiş.");
+    return rejectWithValue("Error getting topic");
+  }
 
-      return topicData;
-   }
-);
+  return topicData;
+});
