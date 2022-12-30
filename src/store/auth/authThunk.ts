01089e0d29f15cbf76d63754e1f78389c0327d72
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-30 16:29:17 +0300

    feat: update auth store

--- src/store/auth/authThunk.ts
+++ src/store/auth/authThunk.ts
@@ -11,7 +11,7 @@ export const authLogin = createAsyncThunk("auth/login", async (payload: { values
   if (user) {
     return { user, session };
   } else {
-    return payload.formikActions.setErrors({ responseMessage: getErrorTranslation(errors?.items[0].code) });
+    return payload.formikActions.setErrors({ responseMessage: getErrorTranslation(errors) });
   }
 });
 
@@ -22,7 +22,7 @@ export const getAuthGrant = createAsyncThunk("auth/getGrant", async (payload, { 
     toast.success("Mail adresiniz doğrulandı.");
     return { user, session };
   } else {
-    toast.error(getErrorTranslation(errors?.items[0].code));
+    toast.error(getErrorTranslation(errors));
     return rejectWithValue("Error getting auth grant");
   }
 });
@@ -38,7 +38,8 @@ export const authRegister = createAsyncThunk(
     if (user) {
       return { user };
     } else {
-      return payload.formikActions.setErrors({ responseMessage: getErrorTranslation(errors?.items[0].code) });
+      payload.formikActions.setErrors({ responseMessage: getErrorTranslation(errors) });
+      rejectWithValue("Error registering");
     }
   }
 );
