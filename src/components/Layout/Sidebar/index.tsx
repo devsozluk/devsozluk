df17d3356dd46956861bbfd4bebdd46febd05c87
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-01-01 12:52:46 +0300

    feat: update layout components

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -2,30 +2,29 @@ import TopicLoader from "@/components/Loading/sidebar";
 import { getPopularTopics } from "@/store/topic/topicThunk";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks";
 import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
-  const location = useLocation();
   const dispatch = useAppDispatch();
-  const {isLoading,topics} = useAppSelector((state) => state.topic);
+  const { isLoading, topics } = useAppSelector((state) => state.topic);
 
   useEffect(() => {
-    dispatch(getPopularTopics())
-  }, [, location]);
+    dispatch(getPopularTopics());
+  }, []);
 
   return (
-    <div className="hidden md:flex flex-col w-[250px] px-5 lg:w-[300px] space-y-3 ml-8 scrollbar scrollbar-w-2 scrollbar-thumb-primary scrollbar-track-slate-700  scrollbar-thumb-rounded  fixed h-screen overflow-y-auto hover:scrollbar-thumb-buttonPrimary rounded">
+    <div className="fixed ml-8 hidden h-screen w-[250px] flex-col space-y-3 overflow-y-auto rounded px-5 scrollbar scrollbar-track-slate-700  scrollbar-thumb-primary  scrollbar-thumb-rounded scrollbar-w-2 hover:scrollbar-thumb-buttonPrimary md:flex lg:w-[300px]">
       {isLoading ? (
-          <TopicLoader />
+        <TopicLoader />
       ) : (
         topics?.map((topic, index) => (
           <Link
             key={index}
-            className="text-base px-2 py-1 break-words hover:bg-buttonPrimary hover:text-white transition-all rounded flex justify-between items-center"
+            className="flex items-center justify-between break-words rounded px-2 py-1 text-base transition-all hover:bg-buttonPrimary hover:text-white"
             to={"/konu/" + topic.slug}
           >
             {topic.title}
-            <span className="text-sm ml-5">{topic?.entryCount}</span>
+            <span className="ml-5 text-sm">{topic?.entryCount}</span>
           </Link>
         ))
       )}
