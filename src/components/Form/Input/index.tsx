a433882854dfb2b5c1626202a0def39e49eadd9f
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-01-05 00:56:37 +0300

    feat: update input component

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,25 +1,22 @@
 import classnames from "classnames";
 import { ErrorMessage, Field } from "formik";
-import React, { HTMLInputTypeAttribute } from "react";
+import React from "react";
 
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder?: string;
   label?: string;
-  type?: HTMLInputTypeAttribute;
   errorText?: string;
   renderLeftIcon?: React.ReactNode;
-  onBlur?: React.ChangeEvent<HTMLInputElement>;
-  onChange?: React.ChangeEvent<HTMLInputElement>;
 }
 
 const Input: React.FC<InputProps> = ({ name, type = "text", errorText, renderLeftIcon, label, placeholder, ...props }) => {
   return (
-    <div className="w-full group">
+    <div className="group w-full">
       <p className="mb-1">{label}</p>
       <div
         className={classnames(
-          "flex items-center border-placeholder border rounded py-4 px-3 space-x-3 text-placeholder focus-within:border-tertiary transition-all",
+          "flex items-center space-x-3 rounded border border-placeholder py-4 px-3 text-placeholder transition-all focus-within:border-tertiary",
           {
             "!border-red-500": errorText,
           }
@@ -27,10 +24,11 @@ const Input: React.FC<InputProps> = ({ name, type = "text", errorText, renderLef
       >
         {renderLeftIcon}
         <Field
-          className="bg-transparent w-full outline-none text-placeholder placeholder:text-placeholder"
+          className="w-full bg-transparent text-placeholder outline-none placeholder:text-placeholder"
           name={name}
           type={type}
           placeholder={placeholder}
+          {...props}
         />
       </div>
       <ErrorMessage className="pt-1 text-sm text-red-500" name={name} component="p" />
