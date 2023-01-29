501ca91cb3f4a43f1b91efa5a6542928827b7b96
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-01-29 19:53:51 +0300

    feat: add web project

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "@/styles/tailwind.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@devsozluk/config/tailwind.config.js");
