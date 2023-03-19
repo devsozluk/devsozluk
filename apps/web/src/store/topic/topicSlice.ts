786fbd9ab1018595f8685cbe2d93b2b245159375
osmandlsmn <dlsmnosman@gmail.com>
2023-03-19 14:12:34 +0300

    fix: build type errors

--- apps/web/src/store/topic/topicSlice.ts
+++ apps/web/src/store/topic/topicSlice.ts
@@ -1,6 +1,5 @@
 import { IEntry, ITopic } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
-import { createTopic, getBySlugTopic, getLatestEntries, getPopularTopics } from "./topicThunk";
 
 interface ITopicState extends ITopic {
   entries: IEntry[];
@@ -21,36 +20,6 @@ const topicSlice = createSlice({
     toggleTopicModal(state) {
       state.isOpenTopicModal = !state.isOpenTopicModal;
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(getBySlugTopic.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(getBySlugTopic.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.topic = action.payload;
-    });
-    builder.addCase(getBySlugTopic.rejected, (state, action) => {
-      state.isLoading = false;
-      // state.errorMessage = action.payload;
-    });
-    builder.addCase(getPopularTopics.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(getPopularTopics.fulfilled, (state, action) => {
-      state.topics = action.payload.result;
-      state.isLoading = false;
-    });
-    builder.addCase(getLatestEntries.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(getLatestEntries.fulfilled, (state, action) => {
-      state.entries = action.payload.result;
-      state.isLoading = false;
-    });
-    builder.addCase(createTopic.fulfilled, (state, action) => {
-      state.isOpenTopicModal = false;
-    });
   },
 });
 
