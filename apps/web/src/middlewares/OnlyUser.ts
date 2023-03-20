e7b10c3ccd434280782bc968d1922afa86fe92a9
osmandlsmn <dlsmnosman@gmail.com>
2023-03-20 17:57:35 +0300

    feat: update middleware

--- apps/web/src/middlewares/OnlyUser.ts
+++ apps/web/src/middlewares/OnlyUser.ts
@@ -2,15 +2,15 @@ import { PropsWithChildren, useEffect } from "react";
 import { useAppSelector } from "@/utils/hooks";
 import { useRouter } from "next/router";
 
-export default function OnlyGuard({ children }: PropsWithChildren): any {
+export default function OnlyUser({ children }: PropsWithChildren): any {
   const router = useRouter();
   const { checkSessionLoading, isLoggedIn } = useAppSelector(
     (state) => state.auth
   );
 
   useEffect(() => {
-    if (!checkSessionLoading && isLoggedIn) {
-      router.push("/");
+    if (!checkSessionLoading && !isLoggedIn) {
+      router.push("/auth/login");
     }
   }, [isLoggedIn, router, checkSessionLoading]);
 
