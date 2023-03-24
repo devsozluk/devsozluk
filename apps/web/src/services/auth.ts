d1c99a1ce12cc77a105c3906acef84f11732d1d3
osmandlsmn <dlsmnosman@gmail.com>
2023-03-24 15:31:04 +0300

    feat: update avatar

--- apps/web/src/services/auth.ts
+++ apps/web/src/services/auth.ts
@@ -62,7 +62,7 @@ export const authApi = createApi({
             data: {
               name,
               user_name: username,
-              avatar_url: `https://api.dicebear.com/5.x/initials/svg?seed=${username}&radius=5&randomizeIds=true&fontWeight=300`,
+              avatar_url: `https://api.dicebear.com/5.x/big-ears-neutral/svg?seed=${username}`,
             },
           },
         });
