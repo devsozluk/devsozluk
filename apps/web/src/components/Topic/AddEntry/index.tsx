72ea2ed0e5380c3536117bcaf0c5490495523170
osmandlsmn <dlsmnosman@gmail.com>
2023-03-26 22:52:38 +0300

    fix: entry created date

--- apps/web/src/components/Topic/AddEntry/index.tsx
+++ apps/web/src/components/Topic/AddEntry/index.tsx
@@ -9,47 +9,60 @@ import { useEffect } from "react";
 import { toast } from "react-hot-toast";
 
 const TopicAddEntry = () => {
-    const initialValues: AddEntryData = { content: "" }
-    const user = useAppSelector((state) => state.auth.user);
-    const { topic } = useAppSelector((state) => state.topic);
+  const initialValues: AddEntryData = { content: "" };
+  const user = useAppSelector((state) => state.auth.user);
+  const { topic } = useAppSelector((state) => state.topic);
 
-    const [addEntry, { status, isLoading, error }] = useAddEntryMutation();
+  const [addEntry, { status, isLoading, error }] = useAddEntryMutation();
 
-    useEffect(() => {
-        if (status === "fulfilled") {
-            toast.success("Başarıyla konuya mesaj gönderildi.");
-        } else if (status === "rejected") {
-            const errorMessage = getErrorFromPayload(error);
-            toast.error(getErrorTranslation(errorMessage));
-        }
-    }, [status]);
-
-    const handleSubmit = (values: AddEntryData, formikActions: any) => {
-        addEntry({
-            content: values.content,
-            topic: topic.id,
-            author: user?.id as string
-        })
-        formikActions.resetForm();
+  useEffect(() => {
+    if (status === "fulfilled") {
+      toast.success("Başarıyla konuya mesaj gönderildi.");
+    } else if (status === "rejected") {
+      const errorMessage = getErrorFromPayload(error);
+      toast.error(getErrorTranslation(errorMessage));
     }
+  }, [status]);
 
-    return (
-        <Formik
-            validationSchema={AddEntrySchema}
-            initialValues={initialValues}
-            onSubmit={handleSubmit}
-            validateOnChange={false}
-            validateOnBlur={false}
+  const handleSubmit = (values: AddEntryData, formikActions: any) => {
+    addEntry({
+      content: values.content,
+      topic: topic.id,
+      author: user?.id as string,
+    });
+    formikActions.resetForm();
+  };
+
+  return (
+    <Formik
+      validationSchema={AddEntrySchema}
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {({ errors, values, setFieldValue, handleSubmit }) => (
+        <TextArea
+          value={values.content}
+          errorMessage={errors.content}
+          onChange={(event) => setFieldValue("content", event.target.value)}
+          placeholder="Göndermek istediğin mesajı yaz."
+          rows={4}
         >
-            {({ errors, values, setFieldValue, handleSubmit }) => (
-                <TextArea value={values.content} errorMessage={errors.content} onChange={(event) => setFieldValue("content", event.target.value)} placeholder="Göndermek istediğin mesajı yaz." rows={4} >
-                    <TextArea.Actions>
-                        <Button loading={isLoading} onClick={() => handleSubmit()} className="ml-auto" size="sm">Gönder</Button>
-                    </TextArea.Actions>
-                </TextArea>
-            )}
-        </Formik>
-    )
-}
+          <TextArea.Actions>
+            <Button
+              loading={isLoading}
+              onClick={() => handleSubmit()}
+              className="ml-auto"
+              size="sm"
+            >
+              Gönder
+            </Button>
+          </TextArea.Actions>
+        </TextArea>
+      )}
+    </Formik>
+  );
+};
 
 export default TopicAddEntry;
