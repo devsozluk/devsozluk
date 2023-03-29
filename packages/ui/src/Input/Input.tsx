f8f2bbd9c4dfb4b4b10cfa0e50016070d05dd525
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-03-29 20:53:19 +0300

    Revert "add custom scrollbar"

--- packages/ui/src/Input/Input.tsx
+++ packages/ui/src/Input/Input.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { cx } from "class-variance-authority";
 
+const baseClasses =
+  "flex items-center space-x-3 rounded border border-placeholder py-4 px-3 text-placeholder transition-all focus-within:border-tertiary";
 const errorClasses = "!border-red-400 !text-red-400";
 
 export interface InputProps
@@ -23,9 +25,6 @@ export const Input: React.FC<InputProps> = ({
   className,
   ...props
 }) => {
-  const baseClasses = `text-sm border outline-none transition-all h-12 border-gray-200 rounded-lg bg-gray-700 border-gray-600 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${
-    renderLeftIcon ? "pl-10" : ""
-  }`;
   const InputClasses = cx(
     className,
     errorMessage ? cx(baseClasses, errorClasses) : baseClasses
@@ -34,11 +33,12 @@ export const Input: React.FC<InputProps> = ({
   return (
     <div className="group w-full">
       <p className="block mb-2 text-sm font-medium text-white">{label}</p>
-      <div className="relative w-full">
-        <div className="absolute w-full inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          {renderLeftIcon}
-        </div>
-        <input className={InputClasses} {...props} />
+      <div className={InputClasses}>
+        {renderLeftIcon}
+        <input
+          className="w-full bg-transparent text-white outline-none placeholder:text-placeholder"
+          {...props}
+        />
       </div>
       <ErrorField errorMessage={errorMessage} />
     </div>
