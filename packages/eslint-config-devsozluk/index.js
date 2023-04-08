ca478748a8bec1f3750352b6f47059c2a33c238f
osmandlsmn <dlsmnosman@gmail.com>
2023-04-08 22:08:20 +0300

    fix: deployment errors

--- packages/eslint-config-devsozluk/index.js
+++ packages/eslint-config-devsozluk/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    extends: ["next", "turbo", "prettier"],
-    rules: {
-      "react/display-name": [2, { "ignoreTranspilerName": true }],
-      "@next/next/no-html-link-for-pages": "off",
-      "react/jsx-key": "off",
-    },
-  };
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    "react/display-name": [2, { ignoreTranspilerName: true }],
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+  },
+};
