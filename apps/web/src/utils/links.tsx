3d809e3fa35ac006d84f6bdb574171eb8d5c5942
osmandlsmn <dlsmnosman@gmail.com>
2023-04-08 15:45:06 +0300

    feat: update dashboard design, new features

---
+++ apps/web/src/utils/links.tsx
@@ -0,0 +1,34 @@
+import { AiFillCompass, AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai";
+
+export type LinkType = "website" | "github" | "twitter" | "linkedin";
+
+export interface Link {
+  name: LinkType;
+  label: string,
+  icon: React.ReactNode;
+}
+
+export const links: Link[] = [
+  {
+    name: "website",
+    label: "Website",
+    icon: <AiFillCompass size={18} />
+  },
+  {
+    name: "github",
+    label: "Github",
+    icon: <AiFillGithub size={18} />
+  },
+  {
+    name: "twitter",
+    label: "Twitter",
+    icon: <AiFillTwitterCircle size={18} />
+  },
+  {
+    name: "linkedin",
+    label: "Linkedin",
+    icon: < AiFillLinkedin size={18} />
+  }
+]
+
+export default links;
