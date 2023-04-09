ebaa520612f16bd2665e133e9cbfb366b6e5fb76
osmandlsmn <dlsmnosman@gmail.com>
2023-04-09 16:46:43 +0300

    feat: add tabs, dropdown reusable components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,7 +1,8 @@
-import * as React from "react";
-export { Button, type ButtonProps } from "./Button/Button";
-export { IconButton, type IconButtonProps } from "./IconButton/IconButton";
-export { Spinner, type SpinnerProps } from "./Spinner/Spinner";
-export { Input, type InputProps } from "./Input/Input";
 export { Alert, type AlertProps } from "./Alert/Alert";
+export { Button, type ButtonProps } from "./Button/Button";
+export { Dropdown, type DropdownProps } from "./Dropdown/Dropdown";
+export { IconButton, type IconButtonProps } from "./IconButton/IconButton";
+export { Input, type InputProps } from "./Input/Input";
+export { Spinner, type SpinnerProps } from "./Spinner/Spinner";
+export { Tabs, type TabType, type TabsProps } from "./Tabs/Tabs";
 export { TextArea, type TextAreaProps } from "./TextArea/TextArea";
