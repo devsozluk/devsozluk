b89aa9d536d2004dc81e01996e5bfae47f1d1829
osmandlsmn <dlsmnosman@gmail.com>
2023-04-14 22:37:55 +0300

    feat: update layout

--- apps/web/src/components/Layout/MainLayout.tsx
+++ apps/web/src/components/Layout/MainLayout.tsx
@@ -6,9 +6,9 @@ export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex justify-center">
       <Header />
-      <main className="mt-44 flex py-7 md:mt-16 container px-8 2xl:px-0">
+      <main className="mt-44 flex py-7 md:mt-16 container px-8 lg:px-8 xl:px-0">
         <Sidebar />
-        <div className="mx-8 h-full w-full md:ml-[300px] md:mr-8 lg:ml-[360px]">
+        <div className="mx-8 h-full w-full md:ml-[300px] lg:ml-[360px]">
           {children}
         </div>
       </main>
