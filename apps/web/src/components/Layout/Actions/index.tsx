b53e7a624945fa682d28cbfcc18410afa996befa
osmandlsmn <dlsmnosman@gmail.com>
2023-05-04 22:34:00 +0300

    fix: topic page line champ

--- apps/web/src/components/Layout/Actions/index.tsx
+++ apps/web/src/components/Layout/Actions/index.tsx
@@ -5,6 +5,7 @@ import { useRef, useState } from "react";
 import { useHover } from "usehooks-ts";
 import { useMediaQuery } from "react-responsive";
 import useActions, { IMenu } from "./Actions.menu";
+import { ClickAwayListener } from "@devsozluk/ui";
 
 const Actions = () => {
   const menus = useActions();
@@ -21,41 +22,44 @@ const Actions = () => {
   );
 
   const handleOpenActions = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+    setIsOpen(true);
+  };
+
+  const handleCloseActions = () => {
+    setIsOpen(false);
   };
 
   return (
-    <div
-      ref={hoverRef}
-      onClick={handleOpenActions}
-      className="fixed right-8 bottom-8 group"
-    >
-      <div id="speed-dial-menu-square" className={classes}>
-        {menus.map((action, index) => (
-          <Actions.Item {...action} key={index} />
-        ))}
-      </div>
-      <button
-        type="button"
-        className="flex items-center justify-center text-white bg-blue-700 rounded-lg w-12 h-12 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
-      >
-        <svg
-          aria-hidden="true"
-          className="w-8 h-8 transition-transform group-hover:rotate-45"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+    <ClickAwayListener onClickAway={handleCloseActions}>
+      <div ref={hoverRef} className="fixed right-8 bottom-8 group">
+        <div id="speed-dial-menu-square" className={classes}>
+          {menus.map((action, index) => (
+            <Actions.Item {...action} key={index} />
+          ))}
+        </div>
+        <button
+          type="button"
+          onClick={handleOpenActions}
+          className="flex items-center justify-center text-white bg-blue-700 rounded-lg w-12 h-12 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          ></path>
-        </svg>
-      </button>
-    </div>
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 transition-transform group-hover:rotate-45"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </ClickAwayListener>
   );
 };
 
