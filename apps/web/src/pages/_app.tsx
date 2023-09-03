36dd4c69f426df5cac8cab95faae473552deb8fc
osmandlsmn <dlsmnosman@gmail.com>
2023-09-03 16:24:51 +0300

    fix: embed position

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -24,7 +24,6 @@ const App = ({ Component, pageProps }: Props) => {
       <DefaultSeo {...seo} />
       <Provider store={store}>
         <RootLayout>
-          <a href="https://www.producthunt.com/products/devsozluk/reviews?utm_source=badge-product_review&utm_medium=badge&utm_souce=badge-devsozluk" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/product_review.svg?product_id=549647&theme=light" alt="DevS&#0246;zl&#0252;k - Open&#0032;source&#0032;social&#0032;platform&#0032;for&#0032;developers&#0046; | Product Hunt" style={{position: "fixed", right: 50, bottom: 50, width: 250, height: 54}} width="250" height="54" /></a>
           <Component {...pageProps} />
         </RootLayout>
         <Toaster />
