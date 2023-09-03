ce284a824b504af9f91c65e8a26e33e9f640a479
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-09-03 14:56:46 +0300

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -20,6 +20,11 @@ const nextConfig = withPWA({
     ],
     formats: ["image/webp"],
   },
+
+ /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  
   reactStrictMode: true,
 });
 
