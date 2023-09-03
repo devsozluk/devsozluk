440371f42d4fd5438c508fbb4f140f9c66805691
osmandlsmn <dlsmnosman@gmail.com>
2023-09-03 19:24:27 +0300

    fix

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -24,7 +24,6 @@ const App = ({ Component, pageProps }: Props) => {
       <DefaultSeo {...seo} />
       <Provider store={store}>
         <RootLayout>
-          <a href="https://www.producthunt.com/posts/devsozluk?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-devsozluk" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=412768&theme=light" alt="DevS&#0246;zl&#0252;k - Open&#0032;source&#0032;social&#0032;platform&#0032;for&#0032;developers&#0046; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
           <Component {...pageProps} />
         </RootLayout>
         <Toaster />
