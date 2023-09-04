35face4791c7264963fc7a7e7ed8459fd2a3ec6f
osmandlsmn <dlsmnosman@gmail.com>
2023-09-04 09:53:25 +0300

    fix: dashboard sidebar disabled status

--- apps/web/src/components/Layout/Dashboard/Sidebar/index.tsx
+++ apps/web/src/components/Layout/Dashboard/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import useNavigations, { Navigation } from "./Sidebar.navigations";
 
@@ -7,32 +8,29 @@ const Sidebar = () => {
 
   return (
     <ul className="flex w-[220px] flex-col text-sm font-medium gap-y-5 text-gray-400">
-      {
-        navigations.map((navigation) => (
-          <li key={navigation.id}>
-            <h3 className="text-xs uppercase text-gray-500 font-semibold tracking-wider">
-              {navigation.title}
-            </h3>
-            <ul className="flex flex-col mt-2 gap-y-2">
-              {navigation.children.map((item) => (
-                <Sidebar.Item key={item.id} {...item} />
-              ))}
-            </ul>
-          </li>
-        ))
-      }
+      {navigations.map((navigation) => (
+        <li key={navigation.id}>
+          <h3 className="text-xs uppercase text-gray-500 font-semibold tracking-wider">
+            {navigation.title}
+          </h3>
+          <ul className="flex flex-col mt-2 gap-y-2">
+            {navigation.children.map((item) => (
+              <Sidebar.Item key={item.id} {...item} />
+            ))}
+          </ul>
+        </li>
+      ))}
     </ul>
   );
 };
 
-Sidebar.Item = ({ title, link, id, onClick, icon }: Navigation) => {
+Sidebar.Item = ({ title, link, id, onClick, icon, disabled }: Navigation) => {
   const router = useRouter();
   const isFocus = router.pathname.includes(link as string);
 
   const classes = classNames({
     "hover:bg-gray-700/30": !isFocus,
-    "bg-gray-700/30":
-      isFocus,
+    "bg-gray-700/30": isFocus,
   });
 
   const iconClasses = classNames({
@@ -48,16 +46,19 @@ Sidebar.Item = ({ title, link, id, onClick, icon }: Navigation) => {
 
   return (
     <li className="cursor-pointer" key={id}>
-      <a
-        onClick={handleClick}
+      <Link
+        href={link as string}
         className={classNames(
           "flex items-center gap-x-2 py-2 px-2 rounded-md transition-colors text-gray-300 duration-200 ease-in-out",
+          {
+            "!opacity-50 pointer-events-none": disabled,
+          },
           classes
         )}
       >
         <i className={iconClasses}>{icon}</i>
         {title}
-      </a>
+      </Link>
     </li>
   );
 };
